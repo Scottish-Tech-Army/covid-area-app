dcf3efb2e0da84a27cd2f678dea80c281fcc30f5
Keith Gunn <kexith@gmail.com>
2021-03-13 18:01:44 +0000

    add jest framework for expo

---
+++ src/screens/HomeScreen.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import HomeScreen from './HomeScreen'
+
+describe('HomeScreen', function () {
+  it('renders location', () => {
+    expect(true).toBeTruthy()
+  })
+})
