394c119838e7eaa7182b3214e1337d4a15c93ee1
Keith Gunn <kexith@gmail.com>
2021-03-14 12:11:56 +0000

    fix version of react test renderer

--- src/screens/HomeScreen.test.js
+++ src/screens/HomeScreen.test.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { render } from '@testing-library/react-native'
 import HomeScreen from './HomeScreen'
+import { AppStateProvider } from '../lib/appState'
+
+const Providers = ({ children }) => {
+  return <AppStateProvider>{children}</AppStateProvider>
+}
 
 describe('HomeScreen', function () {
   it('renders location', () => {
-    expect(true).toBeTruthy()
+    const { debug } = render(
+      <Providers>
+        <HomeScreen />
+      </Providers>
+    )
+    debug()
   })
 })
