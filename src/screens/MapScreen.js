f3443957ce2d9d1d24048332c89ad7c1dc05dee1
Keith Gunn <kexith@gmail.com>
2021-04-16 18:37:13 +0100

    make map screen full height

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,21 +1,19 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components/native'
 import MapView, { Geojson, PROVIDER_GOOGLE } from 'react-native-maps'
-import Screen from '../components/Screen'
 import { useWindowDimensions } from 'react-native'
 import { useHeaderHeight } from '@react-navigation/stack'
 import edinburgh_geojson from '../../geodata/dist/lads/city-of-edinburgh.json'
-
-const MapBox = styled.View`
-  border: 3px solid darkslategrey;
-  margin-top: 20px;
-`
 
 const Map = styled(MapView)`
   background-color: red;
   height: ${(props) => props.height}px;
 `
 
+const FullScreen = styled.View`
+  flex: 1;
+`
+// TODO: Pass the area so we can show the correct Geojson
 function MapScreen(props) {
   const { navigation, route } = props
   const { height } = useWindowDimensions()
@@ -28,29 +26,26 @@ function MapScreen(props) {
   }, [])
 
   return (
-    <Screen>
-      <MapBox>
-        <Map
-          provider={PROVIDER_GOOGLE}
-          height={height - headerHeight - 40}
-          region={{
-            latitude:
-              edinburgh_geojson.features[0].geometry.coordinates[0][0][1],
-            latitudeDelta: 1,
-            longitude:
-              edinburgh_geojson.features[0].geometry.coordinates[0][0][0],
-            longitudeDelta: 1,
-          }}
-        >
-          <Geojson
-            geojson={edinburgh_geojson}
-            fillColor="rgba(0,0,255,0.1)"
-            strokeColor="#000"
-            strokeWidth={2}
-          />
-        </Map>
-      </MapBox>
-    </Screen>
+    <FullScreen>
+      <Map
+        provider={PROVIDER_GOOGLE}
+        height={height - headerHeight}
+        region={{
+          latitude: edinburgh_geojson.features[0].geometry.coordinates[0][0][1],
+          latitudeDelta: 1,
+          longitude:
+            edinburgh_geojson.features[0].geometry.coordinates[0][0][0],
+          longitudeDelta: 1,
+        }}
+      >
+        <Geojson
+          geojson={edinburgh_geojson}
+          fillColor="rgba(0,0,255,0.1)"
+          strokeColor="#000"
+          strokeWidth={2}
+        />
+      </Map>
+    </FullScreen>
   )
 }
 
